768bdf278097abd193803188facbb8bc840f8b66
Jib <tokin21@outlook.com>
2021-02-28 13:15:34 +0800

    apollo server express setup

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,28 @@ import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import microConfig from './mikro-orm.config';
 import express from 'express';
-
+import { ApolloServer} from 'apollo-server-express';
+import {buildSchema} from 'type-graphql';
+import { HelloResolver } from "./resolvers/hello";
 // asynchronous function 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
     await orm.getMigrator().up();
 
     const app = express();
-    app.get("/", (_, res) => {
-        res.send("hello");
-    })
+ 
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false,
+        }),
+    });
+
+    apolloServer.applyMiddleware({ app });
+
     app.listen(4000, () => {
         console.log('server started on localhost:4000')
-    })
+    });
 };
 
 main().catch((err) => {
